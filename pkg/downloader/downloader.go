e8c0feca64c52019a86d8eca2de6d8c16eb350e0
Daniil Zhuravlev <nakrovati@outlook.com>
2024-05-06 20:26:58 +0400

    refactor: logging

--- pkg/downloader/downloader.go
+++ pkg/downloader/downloader.go
@@ -4,6 +4,7 @@ import (
 	"fapesnap/pkg/utils"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -28,20 +29,19 @@ func (d *Downloader) DownloadPhotos(userName string, min int, max int) error {
 
 	err := utils.ValidateMinMax(min, max)
 	if err != nil {
-		return err
+		return fmt.Errorf("invalid min/max: %w", err)
 	}
 
 	recentPhotoID := max
 	if max == 100000 {
 		if recentPhotoID, err = d.PhotosProvider.GetRecentPhotoID(userName); err != nil {
-			return err
+			return fmt.Errorf("failed to get recent photo ID: %w", err)
 		}
 	}
 
 	downloadDir, err := utils.GetDownloadDirectory(d.PhotosProvider.GetProviderName(), userName)
 	if err != nil {
-		fmt.Println("Error while getting download directory:", err)
-		return err
+		return fmt.Errorf("failed to get download directory: %w", err)
 	}
 
 	for i := recentPhotoID; i >= min; i-- {
@@ -50,30 +50,40 @@ func (d *Downloader) DownloadPhotos(userName string, min int, max int) error {
 			return err
 		}
 
-		d.DownloadPhoto(photoURL, downloadDir)
-		println("Downloaded:", photoURL)
+		err = d.DownloadPhoto(photoURL, downloadDir)
+		if err != nil {
+			log.Println("error while downloading photo:", err)
+			continue
+		}
+
+		fmt.Println("downloaded:", photoURL)
 	}
+
 	return nil
 }
 
-func (d Downloader) DownloadPhoto(src string, dir string) {
+func (d Downloader) DownloadPhoto(src string, dir string) error {
 	resp, err := http.Get(src)
 	if err != nil {
-		return
+		return fmt.Errorf("failed to download photo: %w", err)
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("photo %s not found", src)
+	}
 
 	fileName := filepath.Join(dir, d.PhotosProvider.GetFileName(src))
 	file, err := os.Create(fileName)
 	if err != nil {
-		fmt.Println("Error while creating file:", err)
-		return
+		return fmt.Errorf("failed to create file: %w", err)
 	}
 	defer file.Close()
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		fmt.Println("Error while copying file:", err)
-		return
+		return fmt.Errorf("failed to save photo: %w", err)
 	}
+
+	return nil
 }
--- pkg/utils/getDownloadDirPath.go
+++ pkg/utils/getDownloadDirPath.go
@@ -9,16 +9,15 @@ import (
 
 func GetDownloadDirectory(providerName string, userName string) (string, error) {
 	usr, err := user.Current()
-
 	if err != nil {
 		return "", err
 	}
+
 	downloadDir := filepath.Join(usr.HomeDir, "Downloads", "fapesnap", providerName, userName)
 
 	err = os.MkdirAll(downloadDir, 0755)
 	if err != nil {
-		fmt.Println("Error while creating download directory:", err)
-		return "", err
+		return "", fmt.Errorf("failed to create download directory: %w", err)
 	}
 
 	return downloadDir, nil
