7aed980660029dd019d1e7f133689a5d8a794cb9
Daniil Zhuravlev <nakrovati@outlook.com>
2024-05-06 20:33:30 +0400

    chore: improve error handling in min/max validation

--- pkg/utils/validateMinMax.go
+++ pkg/utils/validateMinMax.go
@@ -3,16 +3,16 @@ package utils
 import "fmt"
 
 func ValidateMinMax(min int, max int) error {
-	if min > max {
-		return fmt.Errorf("min cannot be greater than max")
-	}
-
 	if min < 1 || max < 1 {
 		return fmt.Errorf("min and max cannot be less than 1")
 	}
 
 	if min > 100000 || max > 100000 {
-		return fmt.Errorf("max cannot be greater than 100000")
+		return fmt.Errorf("min abd max cannot be greater than 100000")
+	}
+
+	if min > max {
+		return fmt.Errorf("min (%d) cannot be greater than max (%d)", min, max)
 	}
 
 	return nil
